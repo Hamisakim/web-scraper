7dc01a5da15e7c77014797592a6dc446c9106622
hamisakim <samishakim@gmail.com>
2021-05-27 11:49:57 +0100

    adding ticket function

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ import puppeteer from 'puppeteer'
 // ● the date 
 // ● the price 
 // * need to find way to use the refined music only results 
-//* Need to find a way to get all the .content singular events from the #Content and loop through extracting each one
 //* get elements id from url then can use that as param for requests like DB
-
-
 
 const scrapeForEventsArray = async (url) => {
   try {
@@ -16,8 +13,9 @@ const scrapeForEventsArray = async (url) => {
     const page = await browser.newPage()
     await page.goto(url)
     const contentWrapper = await page.$('#Content')
+   
+    //#region //? Alternate -> page.evaluate instead..
     //__________________________________________________________________________________________________________________
-    //#region //? Alternate page.evaluate instead..
     // return page.evaluate(() => {
     // // get all td elements
     //   const tdList = [...document.querySelectorAll('.content')] 
@@ -60,14 +58,13 @@ const scrapeForEventsArray = async (url) => {
     
     //__________________________________________________________________________________________________________________
     const arrayOfEvents = await contentWrapper.$$eval('.content', (nodes) => nodes.map(item =>{
+      /// For each .content div we can go through the elements
       const tbody = item.querySelector('tbody')
       if (tbody === null){ 
-        return '🔴 No table body data' //! change to null? //? make this throw an error on the front
+        return '🔴 No data event' //! change to null? //? make this throw an error for the front
       } else { 
         const eventTitle =  item.querySelector('h2').innerText
         const eventPageLink = item.querySelector('a').href
-        /// For each .content we can go through the elements
-          
         const allRows = tbody.querySelectorAll('tr')
         const eventDetails = []
           
@@ -89,33 +86,55 @@ const scrapeForEventsArray = async (url) => {
       }
     }))
     await browser.close()
-    arrayOfEvents.shift()
-    // return arrayOfEvents[12]
+    arrayOfEvents.shift() //? removes first duplicate 
+    // return arrayOfEvents[12] //? singular object example
     return arrayOfEvents //!FOR FULL ARRAY
+
   } catch (error) {
     console.log('🟥',error)
   }
 }
-// scrapeProduct('https://www.wegottickets.com/searchresults/adv')
             
 const getArray = async () => {
   const response = await scrapeForEventsArray('https://www.wegottickets.com/searchresults/adv')
-  console.log('🟩',response)
+  // console.log('🟩 array of event objects 🟩',response)
   return response
 }
 getArray()
 
-//todo map through each item and run the function to get ticket pricing from page url
 
+//todo map through each item and run the function to get ticket pricing from page url
 
 const getTicketInfo = async (url = 'https://www.wegottickets.com/event/515342') => {
   try {
     const browser = await puppeteer.launch({    dumpio: true   })
     const page = await browser.newPage()
     await page.goto(url)
+    const contentWrapper = await page.$('form.full-width')
+    const arrayOfTickets = await contentWrapper.$$eval('.BuyBox', (nodes) => nodes
+      .map(item =>{
+        const tbody = item.querySelector('tbody')
+        if (tbody === null){ 
+          return '🔴 No data ticket' //! change to null? //? make this throw an error for the front
+        } else { 
+          const price = item.querySelector('.price').innerText
+          const warning = item.querySelector('.warning').innerText
+          console.log('🟢 warning', warning)
+          console.log('🟢 price', price)
+          const ticketDetail = item.querySelector('h2').innerText
+          console.log('🐝 ~ file: index.js ~ line 127 ~ ticketDetail', ticketDetail)
+
+          const ticketObject = {
+            price,
+            warning,
+            ticketDetail
+          }
+          console.log('🐝 ~ file: index.js ~ line 128 ~ ticketObject', ticketObject)
 
 
-
+        }
+      }))
+    await browser.close()
 
   } catch (error) {
     console.log('🟥🟥', error)
